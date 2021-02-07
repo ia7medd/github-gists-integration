eb541999b00cdcecef447d86fb0c1af54d34a7f8
ia7medd <ahmed.salama59@gmail.com>
2021-02-07 13:07:19 +0400

    minor change

--- src/layout/components/error/error.tsx
+++ src/layout/components/error/error.tsx
@@ -4,7 +4,7 @@ import './error.scss';
 export function ErrorPage() {
   return (
     <div className='error-page'>
-      <img src='attention.png' />
+      <img src='attention.png' alt='404' />
       <p>Ooops! Something went wrong.</p>
     </div>
   );
